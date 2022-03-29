15ebdd92afaf120ee536428ff952b632057196e4
Andrew Richardson <andrew.richardson@kaleido.io>
2022-03-29 17:30:22 -0400

    Add examples folder
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

---
+++ examples/datatypes/index.ts
@@ -0,0 +1,11 @@
+import { FireFly } from '../..';
+
+async function main() {
+  const firefly = new FireFly('http://localhost:5000');
+  const datatypes = await firefly.getDatatypes();
+  console.log(JSON.stringify(datatypes, null, 2));
+}
+
+if (require.main === module) {
+  main().catch((err) => console.error(`Error: ${err}`));
+}
