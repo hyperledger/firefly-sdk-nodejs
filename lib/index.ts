f92c8d1a1cec419ff541af837e89c48f034ebb7e
Andrew Richardson <andrew.richardson@kaleido.io>
2022-03-29 16:57:10 -0400

    Add some initial methods
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,78 @@
-export function firefly() {
-  console.log('FireFly');
+import axios, { AxiosInstance } from 'axios';
+import {
+  FireFlyDatatype,
+  FireFlyDatatypeCreate,
+  FireFlyDatatypeOptions,
+  FireFlyDatatypeRef,
+  FireFlyStatus,
+} from './interfaces';
+
+const CREATE_TIMEOUT = 60000;
+
+function isDefined<T>(obj: T | undefined | null): obj is T {
+  return obj !== undefined && obj !== null;
+}
+
+export class InvalidDatatypeError extends Error {}
+
+export class FireFly {
+  private rootHttp: AxiosInstance;
+  private http: AxiosInstance;
+
+  constructor(url: string, namespace = 'default') {
+    this.rootHttp = axios.create({ baseURL: `${url}/api/v1` });
+    this.http = axios.create({ baseURL: `${url}/api/v1/namespaces/${namespace}` });
+  }
+
+  async getStatus(): Promise<FireFlyStatus> {
+    const response = await this.rootHttp.get<FireFlyStatus>('/status');
+    return response.data;
+  }
+
+  async getDatatypes(): Promise<FireFlyDatatype[]> {
+    const response = await this.http.get<FireFlyDatatype[]>('/datatypes');
+    return response.data;
+  }
+
+  async getDatatype(ref: FireFlyDatatypeRef): Promise<FireFlyDatatype | undefined> {
+    const response = await this.http.get<FireFlyDatatype>(`/datatypes/${ref.name}/${ref.version}`, {
+      validateStatus: (status) => status === 404 || (status >= 200 && status < 300),
+    });
+    return response.status === 404 ? undefined : response.data;
+  }
+
+  async createDatatype(
+    ref: FireFlyDatatypeRef,
+    schema?: any,
+    options?: FireFlyDatatypeOptions,
+  ): Promise<FireFlyDatatype> {
+    const body: FireFlyDatatypeCreate = {
+      name: ref.name,
+      version: ref.version,
+      validator: options?.validator ?? 'json',
+      value: schema,
+    };
+    const response = await this.http.post<FireFlyDatatype>('/datatypes', {
+      params: { confirm: true, ...options },
+      data: body,
+      timeout: CREATE_TIMEOUT,
+    });
+    return response.data;
+  }
+
+  async getOrCreateDatatype(ref: FireFlyDatatypeRef, schema?: any): Promise<FireFlyDatatype> {
+    const existing = await this.getDatatype(ref);
+    if (existing !== undefined) {
+      if (isDefined(schema) || isDefined(existing.value)) {
+        if (JSON.stringify(schema) !== JSON.stringify(existing.value)) {
+          throw new InvalidDatatypeError(
+            `Datatype for ${ref.name}:${ref.version} already exists, but schema does not match!`,
+          );
+        }
+      }
+      return existing;
+    }
+    const created = await this.createDatatype(ref, schema);
+    return created;
+  }
 }
---
+++ lib/interfaces.ts
@@ -0,0 +1,253 @@
+export interface FireFlyRequestOptions {
+  confirm?: boolean;
+}
+
+export interface FireFlyOrganization {
+  id: string;
+  name: string;
+  identity: string;
+  registered: true;
+}
+
+export interface FireFlyMember {
+  identity: string;
+  node?: string;
+}
+
+export interface FireFlyNode {
+  name: string;
+  registered: boolean;
+  id: string;
+}
+
+export interface FireFlyStatus {
+  org: FireFlyOrganization;
+  node: FireFlyNode;
+}
+
+export interface FireFlySubscriptionRef {
+  id: string;
+  name: string;
+  namespace: string;
+}
+
+export interface FireFlySubscription {
+  namespace: string;
+  name: string;
+  transport: string;
+  filter?: {
+    events?: string;
+  };
+  options?: {
+    firstEvent?: string;
+    readAhead?: number;
+  };
+}
+
+export interface FireFlyDatatypeRef {
+  name: string;
+  version: string;
+}
+
+export interface FireFlyDatatypeOptions extends FireFlyRequestOptions {
+  validator?: string;
+}
+
+export interface FireFlyDatatypeCreate extends FireFlyDatatypeRef {
+  validator: string;
+  value: any;
+}
+
+export interface FireFlyDatatype extends FireFlyDatatypeCreate {
+  created: string;
+  hash: string;
+  id: string;
+  message: string;
+  namespace: string;
+}
+
+export interface FireFlyDataInput {
+  datatype?: FireFlyDatatypeRef;
+  value: any;
+}
+
+export interface FireFlyData extends FireFlyDataInput {
+  id: string;
+  created: string;
+  blob?: FireFlyBlob;
+}
+
+export interface FireFlyBlob {
+  hash: string;
+  size: number;
+  name: string;
+}
+
+export interface FireFlyMessageBase {
+  header: {
+    id: string;
+    type: string;
+    txtype: string;
+    author: string;
+    key: string;
+    cid?: string;
+    topics: string[];
+    tag?: string;
+    namespace: string;
+    created: string;
+  };
+}
+
+export enum FireFlyMessageState {
+  STAGED = 'staged',
+  READY = 'ready',
+  PENDING = 'pending',
+  CONFIRMED = 'confirmed',
+  REJECTED = 'rejected',
+}
+
+export interface FireFlyMessage extends FireFlyMessageBase {
+  group: string;
+  state: FireFlyMessageState;
+  confirmed: string;
+  data: FireFlyDataRef[];
+}
+
+export interface FireFlyMessageInput extends Partial<FireFlyMessageBase> {
+  group?: {
+    name?: string;
+    ledger?: string;
+    members?: FireFlyMember[];
+  };
+  data: Partial<FireFlyData>[];
+}
+
+export interface FireFlyDataRef {
+  id: string;
+  hash: string;
+}
+
+export interface FireFlyEvent {
+  id: string;
+  type: string;
+  namespace: string;
+  reference: string;
+  subscription: FireFlySubscriptionRef;
+  message?: FireFlyMessage;
+}
+
+export enum FireFlyTokenPoolType {
+  FUNGIBLE = 'fungible',
+  NONFUNGIBLE = 'nonfungible',
+}
+
+export interface FireFlyTokenPool {
+  id: string;
+  type: string;
+  namespace: string;
+  name: string;
+  protocolId: string;
+}
+
+export interface FireFlyTokenTransfer {
+  type: string;
+  namespace: string;
+  pool: string;
+  key: string;
+  from: string;
+  to: string;
+  tokenIndex: string;
+  uri: string;
+  amount: string;
+  localId: string;
+  protocolId: string;
+  message: string;
+  messageHash: string;
+  tx: {
+    id: string;
+    type: string;
+  };
+}
+
+export interface FireFlyTokensTransferInput {
+  amount: number;
+  to?: string;
+  key?: string;
+  from?: string;
+  pool?: string;
+  message?: FireFlyMessageInput;
+}
+
+export interface FireFlyTokenBalance {
+  pool: string;
+  uri: string;
+  connector: string;
+  namespace: string;
+  key: string;
+  balance: string;
+  updated: string;
+  tokenIndex?: string;
+}
+
+export enum FireFlyOperationStatus {
+  PENDING = 'Pending',
+  SUCCEEDED = 'Succeeded',
+  FAILED = 'Failed',
+}
+
+export interface FireFlyOperation {
+  id: string;
+  namespace: string;
+  type: string;
+  tx: string;
+  created: string;
+  updated: string;
+  status: FireFlyOperationStatus;
+  error?: string;
+}
+
+export interface FireFlyTransaction {
+  id: string;
+  created: string;
+  hash: string;
+  protocolId: string;
+  status: FireFlyOperationStatus;
+  subject: {
+    namespace: string;
+    reference: string;
+    signer: string;
+    type: string;
+  };
+}
+
+export interface FireFlyOptions {
+  enabled: boolean;
+  host?: string;
+  uri?: string;
+  poolName?: string;
+  subscriptionName?: string;
+  username?: string;
+  password?: string;
+}
+
+export interface FireFlyWebSocketOptions {
+  host: string;
+  namespace: string;
+  subscriptionName: string;
+  username?: string;
+  password?: string;
+  reconnectDelay: number;
+  heartbeatInterval: number;
+}
+
+export interface FireFlyEventListener {
+  processEvent: (event: FireFlyEvent) => boolean | Promise<boolean>;
+}
+
+export interface MessageListener {
+  processMessage: (message: FireFlyMessage) => boolean | Promise<boolean>;
+}
+
+export interface MemberListener {
+  membersChanged: () => void | Promise<void>;
+}
